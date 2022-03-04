a6a34bac693483ff2a45dfb4d77834d7bb25e1d7
rbk052 <rbk052@users.noreply.github.com>
2022-03-04 06:23:04 +0000

    login and register page linked

--- app/src/main/java/com/studentplanner/MainActivity.java
+++ app/src/main/java/com/studentplanner/MainActivity.java
@@ -2,13 +2,59 @@ package com.studentplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email,password;
+    Button loginBtn;
+    TextView btnToRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email.findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        loginBtn = findViewById((R.id.loginBtn));
+        btnToRegister = findViewById(R.id.btnToLogin);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail,pass;
+
+                mail = email.getText().toString();
+                pass = password.getText().toString();
+
+                if (mail.equals("")) {
+                    Toast.makeText(MainActivity.this, "Email is Blank", Toast.LENGTH_LONG).show();
+                }
+
+                else if (pass.equals("")) {
+                    Toast.makeText(MainActivity.this, "Password is Blank", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    // authentication
+                }
+
+            }
+        });
+        btnToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,Register.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/studentplanner/Register.java
+++ app/src/main/java/com/studentplanner/Register.java
@@ -2,13 +2,62 @@ package com.studentplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
+
+    EditText name, email1, password1;
+    Button registerBtn;
+    TextView btnToLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        name = findViewById(R.id.name);
+        email1 = findViewById(R.id.email1);
+        password1 = findViewById(R.id.password1);
+
+        registerBtn = findViewById(R.id.registerBtn);
+        btnToLogin = findViewById(R.id.btnToLogin);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Name, Email1, Password1;
+
+                Name = name.getText().toString();
+                Email1 = email1.getText().toString();
+                Password1 = password1.getText().toString();
+
+                if (Name.equals("")) {
+                    Toast.makeText(Register.this, "Name is Blank", Toast.LENGTH_SHORT).show();
+                }
+                if (Email1.equals("")) {
+                    Toast.makeText(Register.this, "Email is Blank", Toast.LENGTH_SHORT).show();
+                }
+                if (Password1.equals("")) {
+                    Toast.makeText(Register.this, "Password is Blank", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent R = new Intent(Register.this,MainActivity.class);
+                startActivity(R);
+                finish();
+            }
+        });
+
     }
 }
